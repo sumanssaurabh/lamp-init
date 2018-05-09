73d27e203e3c8753c708f269299a603a9fbae2e9
Suman Saurabh <30201452+sumanssaurabh@users.noreply.github.com>
2018-05-09 23:56:13 +0530

    first commit

---
+++ clean.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+#Apache
+echo -e "Removing Apache"
+apt remove -y apache2 
+
+#MYSQL
+echo -e "Removing MYSQL"
+apt-get remove -y mysql-*
+apt-get purge -y mysql-*
+
+#PHP
+apt remove -y php libapache2-mod-php php-mysql \
+php-cli php-common php-xml php-gd \
+php-curl php-mbstring php-xmlrpc \
+php-imagick php-pspell php-imap
+apt-get autoremove -y
+apt-get autoclean -y
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+PASSWORD=
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+#Apache
+echo -e "Installing Apache\n"
+apt update
+apt install -y apache2
+
+#MYSQL
+echo -e "Installing Mysql\n"
+DEBIAN_FRONTEND=noninteractive apt install mysql-server -y
+mysqladmin -u root password $PASSWORD
+#Run the MySQL Secure Installation wizard
+mysql_secure_installation <<EOF
+$PASSWORD
+n
+y
+y
+y
+y
+EOF
+#PHP
+echo -e "Installing PHP"
+apt install -y php libapache2-mod-php php-mysql
+#installing php-extensions
+apt install -y php-cli php-common php-xml php-gd \
+php-curl php-mbstring php-xmlrpc \
+php-imagick php-pspell php-imap
